32b591296a433acccc4e6a10f36a092c32117eff
NahidAhmed47 <nahidahmedsd47@gmail.com>
2023-03-14 07:26:04 +0600

    payment modal bug fixed

--- handleCartOperations.js
+++ handleCartOperations.js
@@ -28,7 +28,6 @@ const displayCartItems=()=>{
     const cartItemsContainer = document.getElementById("cart-items");
     const cartItems=getItemsFromStorage()
     cartItemsContainer.innerHTML="";
-    console.log(cartItems);
     cartItems?.forEach((item)=>{
         const { name, property_type, images,number_of_reviews,price,_id}=item
         cartItemsContainer.innerHTML += `
@@ -37,7 +36,7 @@ const displayCartItems=()=>{
         <td><span> <i onclick='deleteItemFromCart(${_id})' class="mx-2 bi bi-trash3 text-danger"></i>
         </span> 
         <span> 
-        <i class="text-success bi bi-credit-card-fill" onclick='handlePaymentInfo(${_id})' data-bs-toggle="modal" data-bs-target="#paymenModal" ></i> 
+        <i class="text-success bi bi-credit-card-fill" onclick='handlePaymentInfo(${_id})' data-bs-toggle="modal" data-bs-target="#paymentModal" ></i> 
         </span></td>
        
         </tr>
@@ -50,7 +49,7 @@ displayCartItems()
 
 const deleteItemFromCart=(id)=>{
     const cartItems=getItemsFromStorage()
-    const filteredItems=cartItems.filter((item)=>item._id==id)
+    const filteredItems=cartItems.filter((item)=>parseInt(item._id)!==id)
     localStorage.setItem('savedCart', JSON.stringify(filteredItems))
     displayCartItems()
 }
