09af4a24b33fb5897792bf24dfb20ff6b5db94a0
NahidAhmed47 <nahidahmedsd47@gmail.com>
2023-03-14 06:40:59 +0600

    random modal bugs fixed

--- taglineGenarator.js
+++ taglineGenarator.js
@@ -15,29 +15,26 @@ function generateTag(arr, interval) {
   setInterval(() => {
     const randomIndex = Math.floor(Math.random() * arr.length);
     const randomTagLine = tagLines[randomIndex];
-    document.getElementById("taglines").innerText = randomTagLine;
+    document.getElementById("tagline").innerText = randomTagLine;
   }, interval);
 }
 
-generateTag(tagLines, 2000);
+generateTag(tagLines, 3000);
 
 document
   .getElementById("random-room-btn")
   .addEventListener("click", async function () {
-    console.log("hello world");
     const data = await fetch(`ROOMS.json`);
     const result = await data.json();
     const randomIndex = Math.floor(Math.random() * 10);
     const modalBody = document.getElementById("random-room-info-modal-body");
-    const { name, summary, property_type, images, review_scores } = result[9];
-    
-    
+    const { name, summary, property_type, images, review_scores } = result[randomIndex];
 
     const reviews = document.createElement("ol");
-    if (review_scores.scores) {
+    if (review_scores?.scores) {
       reviews.setAttribute('class',"list-group")
 
-      object.keys(review_scores.scores).forEach((key) => {
+      Object.keys(review_scores?.scores).forEach((key) => {
         reviews.innerHTML += `
         <li class="list-group-item list-group-item-info d-flex justify-content-between align-items-center">
         ${key} : 
@@ -48,12 +45,11 @@ document
     } else {
       reviews.innerHTML = "No reviews found";
     }
-
     
     modalBody.innerHTML = `
     <div class="col">
     <div class="card h-100">
-      <img src=${images.picture_url} class="card-img-top " alt="..." style=" height: 300px;
+      <img src="${images?.picture_url}" class="card-img-top " alt="..." style=" height: 300px;
       object-fit: fill;">
       <div class="card-body">
         <h5 class="card-title">${name}</h5>
@@ -61,7 +57,8 @@ document
         <p class="card-text">${summary}</p>
         <div id='review-score'>
       Review Scores :
-      ${review_scores.scores.review_scores_accuracy}
+      <div id="show-reviews">
+      </div>
     </div>
       </div>
       <button class="btn btn-info btn-lg"  role="button"
@@ -69,7 +66,7 @@ document
     </div>
    </div>
     `;
-    
+    document.getElementById('show-reviews').appendChild(reviews)
   });
 
 
